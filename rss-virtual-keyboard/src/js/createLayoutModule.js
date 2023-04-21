07f1bd60c85d5dfe5381edb611f05a060585d8db
ADyBaH <Brandon4@mail.ru>
2023-04-21 18:42:20 +0300

    feat: add ignor for button

--- rss-virtual-keyboard/src/js/createLayoutModule.js
+++ rss-virtual-keyboard/src/js/createLayoutModule.js
@@ -1,18 +1,22 @@
 import createNode from './utils/createNodeModule';
 import { createButtons, addedEvent } from './createButtonsModule';
-// import {
-//   setLanguageToLocalStorage,
-//   getLanguageToLocalStorage,
-// } from './utils/getLocalStorage';
+import {
+  // setLanguageToLocalStorage,
+  getLanguageToLocalStorage,
+} from './utils/getLocalStorageModule';
 import jsonButtons from './keyboard.json';
 
 const root = createNode({ className: 'root', parent: document.body });
 const header = createNode({ tag: 'header', className: 'header', parent: root });
 
-// const keyboardState = {
-//   languageKeyboard: getLanguageToLocalStorage(),
-//   capslock: false,
-// };
+const keyboardState = {
+  languageKeyboard: getLanguageToLocalStorage(),
+  capslock: false,
+  ignoreAddedTextButton: [
+    'Backspace', 'Tab', 'Delete', 'CapsLock', 'Enter', 'ShiftLeft',
+    'ShiftRight', 'ControlLeft', 'MetaLeft', 'AltLeft', 'Space', 'AltRight', 'ControlRight',
+  ],
+};
 
 createNode({
   tag: 'h1',
@@ -23,8 +27,7 @@ createNode({
 
 const main = createNode({ tag: 'main', className: 'main', parent: root });
 
-// const textarea =
-createNode({
+const divForText = createNode({
   tag: 'div',
   className: 'main__textBlock',
   // attr: {
@@ -37,26 +40,35 @@ const keyboardBlock = createNode({
   className: 'main__keyboardBlock',
   parent: main,
 });
-// const arrayButtons =
-createButtons({
+const arrayButtons = createButtons({
   jsonKey: jsonButtons.en.keys,
   jsonKeyCapslock: jsonButtons.en.keysCapslock,
   jsonKeyShift: jsonButtons.en.keysShift,
-  jsonKeyCode: jsonButtons.en.keysCode,
+  jsonKeyCode: jsonButtons.keysCode,
   parentNode: keyboardBlock,
 });
 
-// function buttonLog(event) {
-//   console.log(event);
-// }
+function clickOnButton(event) {
+  if (!keyboardState.ignoreAddedTextButton.includes(event.target.dataset.keycode)) {
+    divForText.textContent += event.target.textContent;
+  }
+}
 function buttonKeyDown(event) {
-  const node = document.querySelector(`[data-keycode = "${event.code}"]`);
+  if (!jsonButtons.keysCode.includes(event.code)) return;
+
+  const node = document.querySelector(`[data-keycode = '${event.code}']`);
+  if (!keyboardState.ignoreAddedTextButton.includes(node.dataset.keycode)) {
+    divForText.textContent += node.textContent;
+  }
+
   node.classList.add('activeButton');
 }
 function buttonKeyUp(event) {
-  const node = document.querySelector(`[data-keycode = "${event.code}"]`);
+  if (!jsonButtons.keysCode.includes(event.code)) return;
+
+  const node = document.querySelector(`[data-keycode = '${event.code}']`);
   node.classList.remove('activeButton');
 }
-// addedEvent({ nodesArray: arrayButtons, callback: buttonLog, event: 'click' });
+addedEvent({ nodesArray: arrayButtons, callback: clickOnButton, event: 'click' });
 addedEvent({ nodesArray: [document.body], callback: buttonKeyDown, event: 'keydown' });
 addedEvent({ nodesArray: [document.body], callback: buttonKeyUp, event: 'keyup' });
