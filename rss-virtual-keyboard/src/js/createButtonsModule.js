3ba3e9352c313bcf814f518cdeb7df1e24005349
ADyBaH <Brandon4@mail.ru>
2023-04-21 13:53:04 +0300

    feat: add createButtonsModule.js

---
+++ rss-virtual-keyboard/src/js/createButtonsModule.js
@@ -0,0 +1,25 @@
+import createNode from './utils/createNodeModule';
+
+export function createButtons(
+  {
+    jsonKey, jsonKeyCapslock, jsonKeyShift, jsonKeyCode, parentNode,
+  },
+) {
+  return jsonKey.map((_, index) => createNode({
+    tag: 'button',
+    className: 'keyboardBlock_key',
+    textContent: jsonKey[index],
+    attr: {
+      'data-key': jsonKey[index],
+      'data-keycapslock': jsonKeyCapslock[index],
+      'data-keyshift': jsonKeyShift[index],
+      'data-keycode': jsonKeyCode[index],
+    },
+    parent: parentNode,
+  }));
+}
+
+export function addedEvent({ callback, nodesArray, event }) {
+  if (!nodesArray && !event) return;
+  nodesArray.forEach((node) => node.addEventListener(event, callback));
+}
