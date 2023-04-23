d0bb473d856b74cf6feb060e1e4c6e631c3b6afb
ADyBaH <Brandon4@mail.ru>
2023-04-23 13:17:45 +0300

    feat: add cache for setToLocalStorage

--- rss-virtual-keyboard/src/utils/local-storage.js
+++ rss-virtual-keyboard/src/utils/local-storage.js
@@ -1,10 +1,10 @@
 export function setToLocalStorage(key, language) {
-  localStorage.setItem(key, language);
+  localStorage.setItem(`${key}(ADyBaH4uK)`, language);
   return language;
 }
 
 export function getLanguageToLocalStorage() {
-  const language = localStorage.getItem('languageKeyboard');
+  const language = localStorage.getItem('languageKeyboard(ADyBaH4uK)');
   if (language) { return language; }
   return setToLocalStorage('languageKeyboard', 'en');
 }
