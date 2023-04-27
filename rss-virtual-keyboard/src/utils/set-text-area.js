ff745209395256c12cd1f030e1e4934ddbbc3909
ADyBaH <Brandon4@mail.ru>
2023-04-27 20:29:09 +0300

    fix: name on set-text-area

--- rss-virtual-keyboard/src/utils/set-text-area.js
+++ rss-virtual-keyboard/src/utils/set-text-area.js
@@ -1,6 +1,6 @@
 import { changeString } from './change-string';
 
-export function setTextArea(node, cursorRange, addedValue, addedRange = 0, action = 'added') {
+export function setTextArea(node, cursorRange, addedValue, addedRange = 0, action = 'addCharacter') {
   const textArea = node;
   textArea.value = changeString(textArea.value, addedValue, cursorRange, action);
   textArea.setSelectionRange(addedRange, addedRange);
