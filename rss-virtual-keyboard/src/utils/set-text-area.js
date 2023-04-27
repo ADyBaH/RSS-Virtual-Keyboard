4312128fad112234de2b2a894dd6a4768c73fba0
ADyBaH <Brandon4@mail.ru>
2023-04-27 20:10:54 +0300

    refactor: function setTextArea

--- rss-virtual-keyboard/src/utils/set-text-area.js
+++ rss-virtual-keyboard/src/utils/set-text-area.js
@@ -2,7 +2,6 @@ import { changeString } from './change-string';
 
 export function setTextArea(node, cursorRange, addedValue, addedRange = 0, action = 'added') {
   const textArea = node;
-  const textCursor = cursorRange;
-  textArea.value = changeString(textArea.value, addedValue, textCursor, action);
+  textArea.value = changeString(textArea.value, addedValue, cursorRange, action);
   textArea.setSelectionRange(addedRange, addedRange);
 }
